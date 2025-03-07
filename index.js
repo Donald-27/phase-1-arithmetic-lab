b7cbf6f97ab5e09a5aaeae142622bcaa2e778380
Donald-27 <+>
2025-03-07 16:36:36 +0300

    modified index.js

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const num3 = 28;
+const num4 = 6;
+
+// to equal 62
+const multiply = num1 * num2; // 62
+
+// to createa random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1; // Generates a random number between 1 and 100
+
+// to find the remainder
+const mod = num3 % num4; 
+
+// to get highest number
+const max = Math.max(1, 5, 10, 15, 20); 
